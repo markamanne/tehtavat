e7d5c068cb1e927584062cbe9ded27ff9765e03a
aleksimanssila <aleksi.manssila@gmail.com>
2026-02-04 14:11:34 +0200

    tehtava 2

---
+++ viikko1/tehtava2.py
@@ -0,0 +1,11 @@
+import math
+
+# Kysytään säde ja muutetaan se heti tekstistä luvuksi (float)
+sade_str = input("Anna ympyrän säde: ")
+sade = float(sade_str)
+
+# Lasketaan pinta-ala: pii * säde toiseen potenssiin
+pinta_ala = math.pi * (sade ** 2)
+
+# Tulostetaan vastaus
+print("Ympyrän pinta-ala on: " + str(pinta_ala))
